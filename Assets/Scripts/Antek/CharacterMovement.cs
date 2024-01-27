e0e29e040c8f47e04ec033934ace928fef599722
TED-exe <116170985+TED-exe@users.noreply.github.com>
2024-01-27 22:40:14 +0100

    fuckingrat move

--- Assets/Scripts/Antek/CharacterMovement.cs
+++ Assets/Scripts/Antek/CharacterMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
@@ -12,7 +13,7 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float rotationSpeed;
 
     [SerializeField] private bool isFuckingRat;
-    private CharacterController characterController;
+    [SerializeField] private Transform waiterTransform;
     private Rigidbody rb;
     public Vector2 playerDirection;
 
@@ -23,28 +24,46 @@ public class CharacterMovement : MonoBehaviour
         controller.Fucking_Controller.Enable();
 
     }
-    private void LateUpdate()
+    private void FixedUpdate()
     {
         if (isFuckingRat)
-            playerDirection = controller.Fucking_Controller.Movement_FuckingRat.ReadValue<Vector2>();
-        else
-            playerDirection = controller.Fucking_Controller.Movement_Waiter.ReadValue<Vector2>();
-
-        if (playerDirection != Vector2.zero)
         {
+            playerDirection = controller.Fucking_Controller.Movement_FuckingRat.ReadValue<Vector2>();
+            var horizontal = playerDirection.y;//x
+            var vertical = playerDirection.x;//z
+
+            var moveDirection = waiterTransform.forward * horizontal + waiterTransform.right * vertical;
+            if (playerDirection != Vector2.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            }
+
+            rb.velocity = moveDirection * moveSpeed;
+            Debug.DrawRay(transform.position, moveDirection, Color.red);
+
+        }
+        else
+        {
+            playerDirection = controller.Fucking_Controller.Movement_Waiter.ReadValue<Vector2>();
             Vector3 inputDirection = new Vector3(playerDirection.x, 0f, playerDirection.y);
             Quaternion targetRotation = Quaternion.LookRotation(inputDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * playerDirection.magnitude * Time.fixedDeltaTime);
+
+
+            //movement
+            Quaternion rotation = Quaternion.Euler(transform.forward);
+            Vector3 moveDirection = rotation * (transform.forward * moveSpeed * playerDirection.magnitude);
+            rb.velocity = moveDirection;
+
+            Debug.DrawRay(transform.position, moveDirection, Color.blue);
+            Debug.Log(playerDirection);
+            Vector3 inputDirection_debug = new Vector3(playerDirection.x, 0f, playerDirection.y);
+            Debug.DrawRay(transform.position, transform.forward, Color.green);
+            Debug.DrawRay(transform.position, inputDirection_debug, Color.red);
         }
 
-        //movement
-        Vector3 moveDirection = transform.forward * moveSpeed * playerDirection.magnitude;
-        rb.velocity = moveDirection;
 
-
-        Vector3 inputDirection_debug = new Vector3(playerDirection.x, 0f, playerDirection.y);
-        Debug.DrawRay(transform.position, transform.forward, Color.green);
-        Debug.DrawRay(transform.position, inputDirection_debug, Color.red);
     }
 }
 
