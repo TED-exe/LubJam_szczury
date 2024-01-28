d0ceae8385c11e376f4ab33145f931b11db5f9e4
antek1085 <antek1085@o2.pl>
2024-01-28 15:21:13 +0100

    prefabs

--- Assets/UI/MenuController.cs
+++ Assets/UI/MenuController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private GameObject menu;
+    [SerializeField] private Animation animator;
 
     public void StartGame()
     {
-        
+        animator.Play("CameraAnimation");
     }
 
     public void Exit()
